69ce0d5047fcf4f60ebadf6cbc6c4993595d05f0
espinoleandroo <espinoleandro@gmail.com>
2022-07-14 01:54:37 +0000

    calculos de variables

---
+++ Platzi-CursoDeFundamentosDeR/calculos.R
@@ -0,0 +1,14 @@
+x <- 11
+x
+
+Oficina <- 7
+Platzi <- 1
+Transport <- 1.5
+Tiempo <- Oficina + Platzi + Transport
+Tiempo
+
+Parcial_1 <- 7
+Parcial_2 <- 9
+Parcial_3 <- 10
+Promedio <- (Parcial_3 + Parcial_2 + Parcial_1)/3
+Promedio
